2bf36115fa2e8a440f080cc2c5bc98b3aa030364
Vinicius Ferrarini <viniciusferrarini@outlook.com>
2023-06-27 20:55:27 -0300

    - First commit - Add VpcStack - Add ClusterStack

---
+++ src/main/java/com/myorg/VpcStack.java
@@ -0,0 +1,28 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+
+public class VpcStack extends Stack {
+
+    private final Vpc vpc;
+
+    public VpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public VpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        vpc = Vpc.Builder.create(this, "vpc-one")
+                .maxAzs(2)
+                .natGateways(0)
+                .build();
+    }
+
+    public Vpc getVpc() {
+        return vpc;
+    }
+}
