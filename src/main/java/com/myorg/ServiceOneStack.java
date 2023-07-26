0582428076c817a517188cd16acc1e7d05bd4682
Vinicius Ferrarini <viniciusferrarini@outlook.com>
2023-07-25 21:53:59 -0300

    - update service version

--- src/main/java/com/myorg/ServiceOneStack.java
+++ src/main/java/com/myorg/ServiceOneStack.java
@@ -38,7 +38,7 @@ public class ServiceOneStack extends Stack {
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
                                 .containerName("aws_project_one")
-                                .image(ContainerImage.fromRegistry("viniciusferrarini/aws_project_one:1.3.0"))
+                                .image(ContainerImage.fromRegistry("viniciusferrarini/aws_project_one:1.3.1"))
                                 .containerPort(8080)
                                 .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
                                                 .logGroup(LogGroup.Builder.create(this, "ServiceOneLogGroup")
